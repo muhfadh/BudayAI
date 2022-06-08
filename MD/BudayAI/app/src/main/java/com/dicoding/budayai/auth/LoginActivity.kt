3d6cfa3594887298903d1771828a6cbbfba10687
belladwi19 <bellamardiana19@webmail.umm.ac.id>
2022-06-08 10:53:23 +0700

    add login page

---
+++ MD/BudayAI/app/src/main/java/com/dicoding/budayai/auth/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.dicoding.budayai.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import com.dicoding.budayai.MainActivity
+import com.dicoding.budayai.R
+import com.dicoding.budayai.databinding.ActivityLoginBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginFragment.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.fragment_login_popup, null)
+
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
+
+            dialog.findViewById<View>(R.id.btn_login)?.setOnClickListener {
+                startActivity(Intent(this, MainActivity::class.java))
+//                dialog.dismiss()
+            }
+        }
+    }
+}
